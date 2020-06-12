f474bddc5e05a1bfd686d8408fc19eec56f191e1
Erick <40528900+NightCrawlerX1@users.noreply.github.com>
2020-06-12 11:22:52 -0300

    Fuck

--- src/main/java/com/github/nigthcrawlerx1/jdacommands/CommandListener.java
+++ src/main/java/com/github/nigthcrawlerx1/jdacommands/CommandListener.java
@@ -28,7 +28,7 @@ public class CommandListener extends ListenerAdapter {
         String prefix, msg = event.getMessage().getContentRaw().toLowerCase();
         if (msg.startsWith(prefix = builder.getPrefix()) || msg.startsWith(prefix = builder.getAlternativePrefix()) || msg.startsWith(event.getJDA().getSelfUser().getAsMention() +" ")) {
             String fprefix = prefix, alias = msg.substring(fprefix.length()).split(" ")[0];
-            ICommand cmd = builder.getCommandManager().getCommands().stream().filter(c -> Arrays.stream(c.getAliases()).anyMatch(x -> x.equals(alias))).findFirst().orElse(null);
+            ICommand cmd = builder.getCommandManager().getCommands().stream().filter(c -> Arrays.asList(c.getAliases()).contains(alias)).findFirst().orElse(null);
             if(cmd == null)
                 return;
             new Thread(() -> {
