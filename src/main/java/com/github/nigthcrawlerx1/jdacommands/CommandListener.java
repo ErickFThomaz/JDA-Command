add92bfdc25c70945e528441dca7431f14d49f82
Erick <40528900+NightCrawlerX1@users.noreply.github.com>
2020-06-14 15:32:38 -0300

    I put a wrong return

--- src/main/java/com/github/nigthcrawlerx1/jdacommands/CommandListener.java
+++ src/main/java/com/github/nigthcrawlerx1/jdacommands/CommandListener.java
@@ -47,7 +47,6 @@ public class CommandListener extends ListenerAdapter {
                             return;
                         }
                     }
-                    return;
                 }
                 if(cmd.perms() != null){
                     if(!commandEvent.getMember().hasPermission(cmd.perms())){
