9642232605604b202cce5e2f176bb8cfd31b410a
Erick <40528900+NightCrawlerX1@users.noreply.github.com>
2020-06-24 19:08:43 -0300

    Change method to get version

--- src/main/java/com/github/nigthcrawlerx1/jdacommands/JDACommandInfo.java
+++ src/main/java/com/github/nigthcrawlerx1/jdacommands/JDACommandInfo.java
@@ -1,11 +1,23 @@
 package com.github.nigthcrawlerx1.jdacommands;
 
+/*
+ *   Copyright 2020 Erick (NightCrawlerX1 / NightCrawlerX)
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
 public class JDACommandInfo {
 
-    public static final String VERSION_MAJOR = "@versionMajor@";
-    public static final String VERSION_MINOR = "@versionMinor@";
-    public static final String VERSION_REVISION = "@versionRevision@";
-    public static final String VERSION_BUILD = "@versionBuild@";
-    public static final String VERSION = VERSION_MAJOR.startsWith("@") ? "beta" : String.format("%s.%s.%s_%s", VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_BUILD);
-
+    public static String VERSION = "@version@";
+    public static final String GIT_REVISION = "@revision@";
 }
