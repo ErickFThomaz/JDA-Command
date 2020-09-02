8fbd174046769f59e04f46305b4115c1b84983b7
Petrus <petrus_ant@hotmail.com>
2020-09-02 02:50:41 -0300

    Improve Cooldown performance by using a HashMap instead of an ArrayList.

--- src/main/java/com/github/nigthcrawlerx1/jdacommands/command/cooldown/CooldownHandler.java
+++ src/main/java/com/github/nigthcrawlerx1/jdacommands/command/cooldown/CooldownHandler.java
@@ -3,57 +3,44 @@ package com.github.nigthcrawlerx1.jdacommands.command.cooldown;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CooldownHandler {
 
-    private static ArrayList<Cooldown> cooldowns;
+    private HashMap<String, Cooldown> mapOfCooldowns = new HashMap<>();
 
     public CooldownHandler(){
         Logger logger = LoggerFactory.getLogger(CooldownHandler.class);
         logger.debug("Inicando sistema de cooldown");
-        cooldowns = new ArrayList<>();
     }
-
 
     public boolean hasCooldown(String guild, String user, String command){
-        for(Cooldown cooldown : cooldowns){
-            if(cooldown.getGuildId().equals(guild)){
-                if(cooldown.getGuildId().equals(user)){
-                    if (cooldown.getCommand().equals(command)){
-                        if (cooldown.getSeconds() >= (System.currentTimeMillis() / 1000)){
-                            return true;
-                        }else {
-                            cooldowns.remove(cooldown);
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-        return false;
+        Cooldown cooldown = getTheCooldown(guild, user, command);
+        return cooldown != null;
     }
+
     public void setCooldown(String guild, String user, String command, Long time){
         if(!hasCooldown(guild, user, command)){
-            cooldowns.add(new Cooldown(guild, user, (System.currentTimeMillis() / 1000) + time, command));
+            Cooldown cooldown = new Cooldown(guild, user, time, command);
+            mapOfCooldowns.put(cooldown.getIdentifier(), cooldown);
         }
+    }
+
+    public Cooldown getTheCooldown(String guild, String user, String command){
+        String IDENTIFIER = guild + "-" + user + "-" + command;
+        Cooldown cooldown = mapOfCooldowns.get(IDENTIFIER);
+        if (cooldown != null){
+            if (cooldown.isInCooldown()){
+                return cooldown;
+            }else {
+                mapOfCooldowns.remove(IDENTIFIER);
+            }
+        }
+        return null;
     }
 
     public long getCooldown(String guild, String user, String command){
-        for(Cooldown cooldown : cooldowns){
-            if(cooldown.getGuildId().equals(guild)){
-                if(cooldown.getUserId().equals(user)){
-                    if (cooldown.getCommand().equals(command)){
-                        if (cooldown.getSeconds() >= (System.currentTimeMillis() / 1000)){
-                            return cooldown.getSeconds() - (System.currentTimeMillis() / 1000);
-                        }else {
-                            cooldowns.remove(cooldown);
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-        return 0;
+        Cooldown cooldown = getTheCooldown(guild, user, command);
+        return cooldown != null ? cooldown.getTimeRemaining() : 0;
     }
 }
